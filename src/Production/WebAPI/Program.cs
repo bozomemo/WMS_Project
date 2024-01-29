92e862264cccbf943fac299c7da8c47eb5175625
Mehmet Çomak <mehmet.comak@brands-a.com>
2024-01-29 16:26:42 +0300

    program useswagger ui updated

--- src/Production/WebAPI/Program.cs
+++ src/Production/WebAPI/Program.cs
@@ -17,11 +17,14 @@ namespace WebAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
